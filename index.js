ec399310736e0453a4159701e6ee9cb3494cbe22
ppidcard <tvxqlikei@live.cn>
2021-03-29 15:07:47 +1100

    Done

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let message;
+
+function scuberGreetingForFeet(ride){
+  if(ride<400){
+    message = "This one is on me!";
+  }else if(ride>2000 && ride<=2500){
+    message = "I will gladly take your thirty bucks.";
+  }else if(ride>2500){
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+  message = "Ok, sounds good.";
+}else{
+  message = "No go."
+}
+return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous"){
+    message = "Thank you so much.";
+  }else if(tip === "not as generous"){
+    message = "Thank you.";
+  } else {
+    message = "Bye.";
+  }
+      return message;
 }
+
+
+/*
+
+  describe('switchOnCharmFromTip()', function () {
+    it('should return "Thank you so much." if the tip is generous', function () {
+      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+    });
+
+    it('should return "Thank you." if the tip is not as generous', function () {
+      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+    });
+
+    it('should return "Bye." if anything else', function () {
+      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+    });
+  });
+});
+*/
